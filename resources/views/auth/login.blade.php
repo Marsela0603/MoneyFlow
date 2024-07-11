206256a0efb351d059343e99c8874571da3f7fe9
Marsela0603 <marselasela3554@gmail.com>
2024-07-11 19:20:00 +0700

    benerin tammpilan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,8 +57,13 @@
 
               <div class="card mb-3">
 
+              
                 <div class="card-body">
-
+                <div class="pt-3 pb-0"> 
+                <a class="nav-link collapsed" href="/">
+                  <i class="ri-arrow-left-line"></i>
+                </a>
+                </div>
                   <div class="pt-4 pb-2">
                     <h5 class="card-title text-center pb-0 fs-4">Login ke Akun Anda</h5>
                     <p class="text-center small">Masukkan email dan password untuk login</p>
