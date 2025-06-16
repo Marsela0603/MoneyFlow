9a20a67beaa7e7e08d2d61d712ff36f483b79347
Marsela0603 <marselasela3554@gmail.com>
2025-06-16 09:43:26 +0700

    add-controller

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -80,12 +80,6 @@
           </li>
           <li><hr class="dropdown-divider"></li>
 
-          <li>
-            <a class="dropdown-item d-flex align-items-center" href="/profile">
-              <i class="bi bi-person"></i>
-              <span>Profil</span>
-            </a>
-          </li>
           <li><hr class="dropdown-divider"></li>
 
           <li>
